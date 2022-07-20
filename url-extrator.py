7797381dee8c495a6b24a923c5daf2db7d4ca954
Athos Alves De Souza <athos.ahs@gmail.com>
2022-07-19 23:20:59 -0300

    Add files via upload
    Register a "database" as a text file

--- url-extrator.py
+++ url-extrator.py
@@ -37,10 +37,14 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
+
 
 
 
 extrator_url = ExtratorURL('https://bytebank.com.br/cambio?quatidade=100&moedaOrigem=real&moedaDestino=dolar')
 valor_quantidade = extrator_url.get_valor_parametro('quantidade')
 print(valor_quantidade)
+print(len(extrator_url))
 
